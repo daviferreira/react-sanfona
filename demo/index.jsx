a4db8340f9c65974192c03edd9b390952040e131
Davi Ferreira <hi@daviferreira.com>
2015-06-10 00:12:59 +0200

    Add single example to demo

--- demo/index.jsx
+++ demo/index.jsx
@@ -14,6 +14,7 @@ class Demo extends React.Component {
     return (
       <div className="demo-container">
         <h1>react-sanfona</h1>
+
         <Accordion allowMultiple={true} selectedItem={2}>
           {[1, 2, 3, 4, 5].map((item) => {
             return (
@@ -21,6 +22,19 @@ class Demo extends React.Component {
                 <div>
                   {`Item ${ item } content`}
                   {item === 3 ? <p><img src="http://i.giphy.com/nIMpbXH2WfYRi.gif" /></p> : null}
+                </div>
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+
+        <Accordion>
+          {[1, 2, 3, 4, 5].map((item) => {
+            return (
+              <AccordionItem title={`Item ${ item }`} key={item}>
+                <div>
+                  {`Item ${ item } content`}
+                  {item === 3 ? <p><img src="https://cloud.githubusercontent.com/assets/38787/8015584/2883817e-0bda-11e5-9662-b7daf40e8c27.gif" /></p> : null}
                 </div>
               </AccordionItem>
             );
