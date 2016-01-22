780d6a4070134e2ad5b5cb34997a3b3fda1487e2
Mathieu Dumont <mdumont@cortex.bz>
2016-01-22 16:45:15 -0500

    Bundle stuff

--- dist-modules/AccordionItem/index.js
+++ dist-modules/AccordionItem/index.js
@@ -1,14 +1,12 @@
 'use strict';
 
-exports.__esModule = true;
-
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
 var _classnames = require('classnames');
 
@@ -34,119 +32,139 @@ var _AccordionItemTitle = require('../AccordionItemTitle');
 
 var _AccordionItemTitle2 = _interopRequireDefault(_AccordionItemTitle);
 
-var AccordionItem = (function (_Component) {
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var AccordionItem = function (_Component) {
   _inherits(AccordionItem, _Component);
 
   function AccordionItem(props) {
     _classCallCheck(this, AccordionItem);
 
-    _Component.call(this, props);
-    this.state = {
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(AccordionItem).call(this, props));
+
+    _this.state = {
       maxHeight: props.expanded ? 'none' : 0,
       overflow: props.expanded ? 'visible' : 'hidden'
     };
+    return _this;
   }
 
-  AccordionItem.prototype.componentWillMount = function componentWillMount() {
-    this.uuid = _uuid2['default'].v4();
-  };
-
-  AccordionItem.prototype.componentDidMount = function componentDidMount() {
-    this.setMaxHeight();
-  };
-
-  AccordionItem.prototype.componentDidUpdate = function componentDidUpdate(prevProps) {
-    if (prevProps.expanded !== this.props.expanded) {
+  _createClass(AccordionItem, [{
+    key: 'componentWillMount',
+    value: function componentWillMount() {
+      this.uuid = _uuid2.default.v4();
+    }
+  }, {
+    key: 'componentDidMount',
+    value: function componentDidMount() {
       this.setMaxHeight();
     }
-  };
-
-  AccordionItem.prototype.allowOverflow = function allowOverflow() {
-    this.setState({ overflow: 'visible' });
-  };
-
-  AccordionItem.prototype.setMaxHeight = function setMaxHeight() {
-    var bodyNode = _reactDom2['default'].findDOMNode(this.refs.body);
-    var images = bodyNode.querySelectorAll('img');
-
-    if (images.length > 0) {
-      return this.preloadImages(bodyNode, images);
-    }
-
-    this.setState({
-      maxHeight: this.props.expanded ? bodyNode.scrollHeight + 'px' : 0,
-      overflow: 'hidden'
-    });
-  };
-
-  // Wait for images to load before calculating maxHeight
-
-  AccordionItem.prototype.preloadImages = function preloadImages(node, images) {
-    var _this = this;
-
-    var imagesLoaded = 0;
-
-    var imgLoaded = function imgLoaded() {
-      imagesLoaded++;
-
-      if (imagesLoaded === images.length) {
-        _this.setState({
-          maxHeight: _this.props.expanded ? node.scrollHeight + 'px' : 0,
-          overflow: 'hidden'
-        });
+  }, {
+    key: 'componentDidUpdate',
+    value: function componentDidUpdate(prevProps) {
+      if (prevProps.expanded !== this.props.expanded) {
+        this.setMaxHeight();
       }
-    };
-
-    for (var i = 0; i < images.length; i += 1) {
-      var img = new Image();
-      img.src = images[i].src;
-      img.onload = img.onerror = imgLoaded;
     }
-  };
+  }, {
+    key: 'allowOverflow',
+    value: function allowOverflow() {
+      this.setState({ overflow: 'visible' });
+    }
+  }, {
+    key: 'setMaxHeight',
+    value: function setMaxHeight() {
+      var bodyNode = _reactDom2.default.findDOMNode(this.refs.body);
+      var images = bodyNode.querySelectorAll('img');
 
-  AccordionItem.prototype.getProps = function getProps() {
-    var props = {
-      className: _classnames2['default'](['react-sanfona-item', { 'react-sanfona-item-expanded': this.props.expanded }]),
-      role: 'tabpanel'
-    };
+      if (images.length > 0) {
+        return this.preloadImages(bodyNode, images);
+      }
 
-    if (this.props.expanded) {
-      props['aria-expanded'] = true;
-    } else {
-      props['aria-hidden'] = true;
+      this.setState({
+        maxHeight: this.props.expanded ? bodyNode.scrollHeight + 'px' : 0,
+        overflow: 'hidden'
+      });
     }
 
-    return props;
-  };
+    // Wait for images to load before calculating maxHeight
 
-  AccordionItem.prototype.render = function render() {
-    return _react2['default'].createElement(
-      'div',
-      _extends({}, this.getProps(), { ref: 'item' }),
-      _react2['default'].createElement(_AccordionItemTitle2['default'], {
-        title: this.props.title,
-        onClick: this.props.onClick,
-        titleColor: this.props.titleColor,
-        uuid: this.uuid }),
-      _react2['default'].createElement(
-        _AccordionItemBody2['default'],
-        { maxHeight: this.state.maxHeight,
-          overflow: this.state.overflow,
-          ref: 'body',
-          uuid: this.uuid },
-        this.props.children
-      )
-    );
-  };
+  }, {
+    key: 'preloadImages',
+    value: function preloadImages(node, images) {
+      var _this2 = this;
+
+      var imagesLoaded = 0;
+
+      var imgLoaded = function imgLoaded() {
+        imagesLoaded++;
+
+        if (imagesLoaded === images.length) {
+          _this2.setState({
+            maxHeight: _this2.props.expanded ? node.scrollHeight + 'px' : 0,
+            overflow: 'hidden'
+          });
+        }
+      };
+
+      for (var i = 0; i < images.length; i += 1) {
+        var img = new Image();
+        img.src = images[i].src;
+        img.onload = img.onerror = imgLoaded;
+      }
+    }
+  }, {
+    key: 'getProps',
+    value: function getProps() {
+      var props = {
+        className: (0, _classnames2.default)(['react-sanfona-item', { 'react-sanfona-item-expanded': this.props.expanded }]),
+        role: 'tabpanel'
+      };
+
+      if (this.props.expanded) {
+        props['aria-expanded'] = true;
+      } else {
+        props['aria-hidden'] = true;
+      }
+
+      return props;
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        _extends({}, this.getProps(), { ref: 'item' }),
+        _react2.default.createElement(_AccordionItemTitle2.default, {
+          title: this.props.title,
+          onClick: this.props.onClick,
+          titleColor: this.props.titleColor,
+          uuid: this.uuid }),
+        _react2.default.createElement(
+          _AccordionItemBody2.default,
+          { maxHeight: this.state.maxHeight,
+            overflow: this.state.overflow,
+            ref: 'body',
+            uuid: this.uuid },
+          this.props.children
+        )
+      );
+    }
+  }]);
 
   return AccordionItem;
-})(_react.Component);
+}(_react.Component);
 
-exports['default'] = AccordionItem;
+exports.default = AccordionItem;
 
 AccordionItem.propTypes = {
   expanded: _react.PropTypes.bool,
   onClick: _react.PropTypes.func,
-  title: _react.PropTypes.string
+  title: _react.PropTypes.oneOfType([_react.PropTypes.string, _react.PropTypes.node])
 };
-module.exports = exports['default'];
--- dist-modules/AccordionItemBody/index.js
+++ dist-modules/AccordionItemBody/index.js
@@ -1,55 +1,63 @@
 'use strict';
 
-exports.__esModule = true;
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var AccordionItemBody = (function (_Component) {
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var AccordionItemBody = function (_Component) {
   _inherits(AccordionItemBody, _Component);
 
   function AccordionItemBody() {
     _classCallCheck(this, AccordionItemBody);
 
-    _Component.apply(this, arguments);
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(AccordionItemBody).apply(this, arguments));
   }
 
-  AccordionItemBody.prototype.render = function render() {
-    var style = {
-      maxHeight: this.props.maxHeight,
-      overflow: this.props.overflow,
-      transition: 'max-height .3s ease'
-    };
+  _createClass(AccordionItemBody, [{
+    key: 'render',
+    value: function render() {
+      var style = {
+        maxHeight: this.props.maxHeight,
+        overflow: this.props.overflow,
+        transition: 'max-height .3s ease'
+      };
 
-    return _react2['default'].createElement(
-      'div',
-      { 'aria-labelledby': 'react-safona-item-title-' + this.props.uuid,
-        className: 'react-sanfona-item-body',
-        id: 'react-safona-item-body-' + this.props.uuid,
-        style: style },
-      _react2['default'].createElement(
+      return _react2.default.createElement(
         'div',
-        { className: 'react-sanfona-item-body-wrapper' },
-        this.props.children
-      )
-    );
-  };
+        { 'aria-labelledby': 'react-safona-item-title-' + this.props.uuid,
+          className: 'react-sanfona-item-body',
+          id: 'react-safona-item-body-' + this.props.uuid,
+          style: style },
+        _react2.default.createElement(
+          'div',
+          { className: 'react-sanfona-item-body-wrapper' },
+          this.props.children
+        )
+      );
+    }
+  }]);
 
   return AccordionItemBody;
-})(_react.Component);
+}(_react.Component);
 
-exports['default'] = AccordionItemBody;
+exports.default = AccordionItemBody;
 
 AccordionItemBody.propTypes = {
-  maxHeight: _react.PropTypes.oneOfType([_react2['default'].PropTypes.string, _react2['default'].PropTypes.number]),
+  maxHeight: _react.PropTypes.oneOfType([_react2.default.PropTypes.string, _react2.default.PropTypes.number]),
   overflow: _react.PropTypes.string,
   uuid: _react.PropTypes.string
 };
-module.exports = exports['default'];
