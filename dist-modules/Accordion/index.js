fa18928f114c7b1d32758cc23ba3a40555cbedc5
psimyn <psimyn@gmail.com>
2016-03-31 09:05:00 +1100

    update dist to 0.0.9
    add babel-cli as devdep so it doesn't need to be installed globally to build
    use babel-loader for js and jsx

--- dist-modules/Accordion/index.js
+++ dist-modules/Accordion/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+var _classnames = require('classnames');
 
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+var _classnames2 = _interopRequireDefault(_classnames);
 
 var _react = require('react');
 
@@ -16,87 +18,105 @@ var _reactDom = require('react-dom');
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var Accordion = (function (_Component) {
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Accordion = function (_Component) {
   _inherits(Accordion, _Component);
 
   function Accordion(props) {
     _classCallCheck(this, Accordion);
 
-    _Component.call(this, props);
+    var _this = _possibleConstructorReturn(this, Object.getPrototypeOf(Accordion).call(this, props));
 
     var activeItems = !Array.isArray(props.activeItems) ? [props.activeItems] : props.activeItems;
 
-    this.state = { activeItems: activeItems };
+    _this.state = { activeItems: activeItems };
+    return _this;
   }
 
-  Accordion.prototype.componentDidMount = function componentDidMount() {
-    var _this = this;
+  _createClass(Accordion, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
 
-    this.state.activeItems.forEach(function (index) {
-      if (_this.refs['item-' + index]) {
-        _this.refs['item-' + index].allowOverflow();
+      this.state.activeItems.forEach(function (index) {
+        if (_this2.refs['item-' + index]) {
+          _this2.refs['item-' + index].allowOverflow();
+        }
+      });
+
+      // allow overflow for absolute positioned elements inside
+      // the item body, but only after animation is complete
+      _reactDom2.default.findDOMNode(this).addEventListener('transitionend', function () {
+        _this2.state.activeItems.forEach(function (index) {
+          _this2.refs['item-' + index].allowOverflow();
+        });
+      });
+    }
+  }, {
+    key: 'handleClick',
+    value: function handleClick(index) {
+      var newState = {};
+
+      // clone active items state array
+      newState.activeItems = this.state.activeItems.slice(0);
+
+      var position = newState.activeItems.indexOf(index);
+
+      if (position !== -1) {
+        newState.activeItems.splice(position, 1);
+      } else if (this.props.allowMultiple) {
+        newState.activeItems.push(index);
+      } else {
+        newState.activeItems = [index];
       }
-    });
 
-    // allow overflow for absolute positioned elements inside
-    // the item body, but only after animation is complete
-    _reactDom2['default'].findDOMNode(this).addEventListener('transitionend', function () {
-      _this.state.activeItems.forEach(function (index) {
-        _this.refs['item-' + index].allowOverflow();
-      });
-    });
-  };
-
-  Accordion.prototype.handleClick = function handleClick(index) {
-    var newState = {};
-
-    // clone active items state array
-    newState.activeItems = this.state.activeItems.slice(0);
-
-    var position = newState.activeItems.indexOf(index);
-
-    if (position !== -1) {
-      newState.activeItems.splice(position, 1);
-    } else if (this.props.allowMultiple) {
-      newState.activeItems.push(index);
-    } else {
-      newState.activeItems = [index];
+      this.setState(newState);
     }
+  }, {
+    key: 'renderItems',
+    value: function renderItems() {
+      var _this3 = this;
 
-    this.setState(newState);
-  };
+      if (!this.props.children) {
+        return null;
+      }
 
-  Accordion.prototype.renderItems = function renderItems() {
-    var _this2 = this;
+      return this.props.children.map(function (item, index) {
+        var expanded = _this3.state.activeItems.indexOf(index) !== -1;
 
-    if (!this.props.children) {
-      return null;
-    }
-
-    return this.props.children.map(function (item, index) {
-      var expanded = _this2.state.activeItems.indexOf(index) !== -1;
-
-      return _react2['default'].cloneElement(item, {
-        expanded: expanded,
-        key: index,
-        onClick: _this2.handleClick.bind(_this2, index),
-        ref: 'item-' + index
+        return _react2.default.cloneElement(item, {
+          expanded: expanded,
+          key: index,
+          onClick: _this3.handleClick.bind(_this3, index),
+          ref: 'item-' + index
+        });
       });
-    });
-  };
-
-  Accordion.prototype.render = function render() {
-    return _react2['default'].createElement(
-      'div',
-      { className: 'react-sanfona', role: 'tablist' },
-      this.renderItems()
-    );
-  };
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        { className: (0, _classnames2.default)('react-sanfona', this.props.className),
+          role: 'tablist',
+          style: this.props.style },
+        this.renderItems()
+      );
+    }
+  }]);
 
   return Accordion;
-})(_react.Component);
+}(_react.Component);
 
-exports['default'] = Accordion;
+exports.default = Accordion;
+
 
 Accordion.defaultProps = {
   activeItems: [0],
@@ -105,6 +125,7 @@ Accordion.defaultProps = {
 
 Accordion.propTypes = {
   allowMultiple: _react.PropTypes.bool,
-  activeItems: _react.PropTypes.oneOfType([_react.PropTypes.number, _react.PropTypes.array])
+  activeItems: _react.PropTypes.oneOfType([_react.PropTypes.number, _react.PropTypes.array]),
+  className: _react.PropTypes.string,
+  style: _react.PropTypes.object
 };
-module.exports = exports['default'];
--- dist-modules/AccordionItemTitle/index.js
+++ dist-modules/AccordionItemTitle/index.js
@@ -1,52 +1,66 @@
 'use strict';
 
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+var _classnames = require('classnames');
 
-function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+var _classnames2 = _interopRequireDefault(_classnames);
 
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var AccordionItemTitle = (function (_Component) {
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var AccordionItemTitle = function (_Component) {
   _inherits(AccordionItemTitle, _Component);
 
   function AccordionItemTitle() {
     _classCallCheck(this, AccordionItemTitle);
 
-    _Component.apply(this, arguments);
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(AccordionItemTitle).apply(this, arguments));
   }
 
-  AccordionItemTitle.prototype.render = function render() {
-    var style = {
-      cursor: 'pointer',
-      margin: 0,
-      color: this.props.titleColor
-    };
+  _createClass(AccordionItemTitle, [{
+    key: 'render',
+    value: function render() {
+      var style = {
+        cursor: 'pointer',
+        margin: 0,
+        color: this.props.titleColor
+      };
 
-    return _react2['default'].createElement(
-      'h3',
-      { 'aria-controls': 'react-sanfona-item-body-' + this.props.uuid,
-        className: 'react-sanfona-item-title',
-        id: 'react-safona-item-title-' + this.props.uuid,
-        onClick: this.props.onClick,
-        style: style },
-      this.props.title
-    );
-  };
+      return _react2.default.createElement(
+        'h3',
+        { 'aria-controls': 'react-sanfona-item-body-' + this.props.uuid,
+          className: (0, _classnames2.default)('react-sanfona-item-title', this.props.className),
+          id: 'react-safona-item-title-' + this.props.uuid,
+          onClick: this.props.onClick,
+          style: style },
+        this.props.title
+      );
+    }
+  }]);
 
   return AccordionItemTitle;
-})(_react.Component);
+}(_react.Component);
 
-exports['default'] = AccordionItemTitle;
+exports.default = AccordionItemTitle;
+
 
 AccordionItemTitle.propTypes = {
+  className: _react.PropTypes.string,
   onClick: _react.PropTypes.func,
   title: _react.PropTypes.string,
   uuid: _react.PropTypes.string
 };
-module.exports = exports['default'];
