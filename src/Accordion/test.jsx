a071367124874ac9fa2fa9e4cfb17ad58566952e
psimyn <psimyn@gmail.com>
2016-06-16 00:45:39 +1000

    inconsistently update other Accordion tests

--- src/Accordion/test.jsx
+++ src/Accordion/test.jsx
@@ -1,7 +1,5 @@
 import expect from 'unexpected';
 import React from 'react';
-import sd from 'skin-deep';
-import TestUtils from 'react-addons-test-utils';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
@@ -31,146 +29,118 @@ describe('Accordion Test Case', () => {
         </Accordion>
       );
 
-      expect(item.find(AccordionItem).at(0).prop('expanded'), 'to be true');
-      expect(item.find(AccordionItem).at(1).prop('expanded'), 'to be false');
+      expect(item.childAt(0).prop('expanded'), 'to be true');
+      expect(item.childAt(1).prop('expanded'), 'to be false');
     });
 
     it('should accept a activeItems prop', () => {
-      const tree = sd.shallowRender(
+      const item = shallow(
         <Accordion activeItems={1}>
           <AccordionItem title="First" />
           <AccordionItem title="Second" />
         </Accordion>
       );
 
-      vdom = tree.getRenderOutput();
-
-      items = tree.props.children;
-
-      expect(items[0].props.expanded, 'to be false');
-      expect(items[1].props.expanded, 'to be true');
+      expect(item.childAt(0).prop('expanded'), 'to be false');
+      expect(item.childAt(1).prop('expanded'), 'to be true');
     });
 
     it('should accept a string as active item prop', () => {
-      const tree = sd.shallowRender(
+      const item = shallow(
         <Accordion activeItems={'second'}>
           <AccordionItem title="First" slug='first' />
           <AccordionItem title="Second" slug='second' />
         </Accordion>
       )
 
-      vdom = tree.getRenderOutput();
-
-      items = tree.props.children;
-
-      expect(items[0].props.expanded, 'to be false');
-      expect(items[1].props.expanded, 'to be true');
+      expect(item.childAt(0).prop('expanded'), 'to be false');
+      expect(item.childAt(1).prop('expanded'), 'to be true');
     });
 
     it('should keep only one activeItem when allowMultiple is false', () => {
-      const tree = sd.shallowRender(
+      const item = shallow(
         <Accordion activeItems={1}>
           <AccordionItem title="First" />
           <AccordionItem title="Second" />
         </Accordion>
       );
 
-      instance = tree.getMountedInstance();
+      // todo: findWhere or something
+      expect(item.state().activeItems, 'to equal', [1]);
 
-      expect(instance.state.activeItems, 'to equal', [1]);
+      item.childAt(0).simulate('click');
 
-      instance.handleClick(0);
-
-      expect(instance.state.activeItems, 'to equal', [0]);
+      expect(item.update().state().activeItems, 'to equal', [0]);
     });
   });
 
   describe('allowMultiple', () => {
 
     it('should allow multiple expanded items', () => {
-      const tree = sd.shallowRender(
+      const item = shallow(
         <Accordion activeItems={1} allowMultiple={true}>
           <AccordionItem title="First" />
           <AccordionItem title="Second" />
         </Accordion>
       );
 
-      instance = tree.getMountedInstance();
-      vdom = tree.getRenderOutput();
-      items = vdom.props.children;
+      expect(item.childAt(0).prop('expanded'), 'to be false');
+      expect(item.childAt(1).prop('expanded'), 'to be true');
 
-      expect(items[0].props.expanded, 'to be false');
-      expect(items[1].props.expanded, 'to be true');
+      item.childAt(0).simulate('click');
 
-      instance.handleClick(0);
-
-      vdom = tree.getRenderOutput();
-      items = vdom.props.children;
-
-      expect(items[0].props.expanded, 'to be true');
-      expect(items[1].props.expanded, 'to be true');
+      expect(item.update().childAt(0).prop('expanded'), 'to be true');
+      expect(item.update().childAt(1).prop('expanded'), 'to be true');
     });
 
     it('should default to first active item if allowMultiple is false', () => {
-      const tree = sd.shallowRender(
+      const item = shallow(
         <Accordion activeItems={[0, 1]}>
           <AccordionItem title="First" />
           <AccordionItem title="Second" />
         </Accordion>
       );
 
-      vdom = tree.getRenderOutput();
-
-      items = tree.props.children;
-
-      expect(items[0].props.expanded, 'to be true');
-      expect(items[1].props.expanded, 'to be false');
+      expect(item.childAt(0).props().expanded, 'to be true');
+      expect(item.childAt(1).props().expanded, 'to be false');
     });
 
     it('should allow multiple selected indexes of different types', () => {
-      const tree = sd.shallowRender(
+      const item = shallow(
         <Accordion activeItems={[0, 'second']} allowMultiple>
           <AccordionItem title="First" />
           <AccordionItem title="Second" slug="second" />
         </Accordion>
       );
 
-      vdom = tree.getRenderOutput();
-
-      items = tree.props.children;
-
-      expect(items[0].props.expanded, 'to be true');
-      expect(items[1].props.expanded, 'to be true');
+      expect(item.childAt(0).props().expanded, 'to be true');
+      expect(item.childAt(1).props().expanded, 'to be true');
     });
 
     it('should save activeItems on state when allowMultiple is true', () => {
-      const tree = sd.shallowRender(
+      const item = shallow(
         <Accordion activeItems={1} allowMultiple={true}>
           <AccordionItem title="First" />
           <AccordionItem title="Second" />
         </Accordion>
       );
 
-      instance = tree.getMountedInstance();
-
-      expect(instance.state.activeItems, 'to equal', [1]);
+      expect(item.state('activeItems'), 'to equal', [1]);
     });
 
     it('should update activeItems state when clicking on an item', () => {
-      const tree = sd.shallowRender(
+      const item = shallow(
         <Accordion activeItems={1} allowMultiple={true}>
           <AccordionItem title="First" />
           <AccordionItem title="Second" />
         </Accordion>
       );
 
-      instance = tree.getMountedInstance();
+      expect(item.state('activeItems'), 'to equal', [1]);
 
-      expect(instance.state.activeItems, 'to equal', [1]);
+      item.childAt(0).simulate('click');
 
-      instance.handleClick(0);
-
-      expect(instance.state.activeItems, 'to equal', [1, 0]);
+      expect(item.state('activeItems'), 'to equal', [1, 0]);
     });
 
   });
@@ -193,8 +163,8 @@ describe('Accordion Test Case', () => {
       firstItem.simulate('click');
 
       // call item.update() to wait for async event
-      firstItem = item.update().find(AccordionItem).at(0);
-      secondItem = item.update().find(AccordionItem).at(1);
+      firstItem = item.update().childAt(0);
+      secondItem = item.update().childAt(1);
 
       expect(firstItem.prop('expanded'), 'to be false');
       expect(secondItem.prop('expanded'), 'to be false');
