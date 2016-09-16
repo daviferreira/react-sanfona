dd0e784a5e1791a0ce76721949966d63f996771f
Max Nowack <maxnowack@users.noreply.github.com>
2016-09-16 12:50:17 +0200

    export AccordionItemTitle and AccordionItemBody
    to be flexible on extending the existing components

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 export Accordion from './Accordion';
 export AccordionItem from './AccordionItem';
+export AccordionItemTitle from './AccordionItemTitle';
+export AccordionItemBody from './AccordionItemBody';
