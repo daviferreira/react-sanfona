53473a13356e999dd073f3a3117611ca837d1556
Nikita Tarasov <nikita@mygento.ru>
2017-05-04 14:52:24 +0300

    prop-types

--- src/AccordionItem/index.jsx
+++ src/AccordionItem/index.jsx
@@ -1,7 +1,8 @@
 'use strict';
 
 import className from 'classnames';
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import uuid from 'uuid';
 
